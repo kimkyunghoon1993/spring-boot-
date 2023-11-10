74aab4b74505f0e6075f5200c8259f79ad21caa2
김경훈 <kimkyunghoon1993@gmail.com>
2023-11-10 17:26:39 +0900

    2023-11-10 금요일 서버 실행 console 추가

--- src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.springboot_demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            log.info("spring boot 연습 서버 start 완료");
+        };
     }
 
 }
